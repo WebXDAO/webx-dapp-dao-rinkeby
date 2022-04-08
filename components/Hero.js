a95e76b4d7b58328d51105a071b3d5710e4cd573
Abhinav MV <mvabhinav1998@gmail.com>
2022-04-08 23:01:24 +0530

    Add transparent logo

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { ChevronRightIcon } from "@heroicons/react/solid";
 import { useMetamask } from "@thirdweb-dev/react";
-import styles from "../styles/Login.module.css";
+import "../styles/Login.module.css";
 import { Animation } from "./Animation";
 
 const navigation = [
@@ -28,7 +28,7 @@ const Hero = () => {
               <div className="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
                   <span className="sr-only">WebX DAO</span>
-                  <img className="h-8 w-auto sm:h-10" src="/logo.png" alt="" />
+                  <img className="h-16 aspect-video" src="/logoTransparent.png" alt="" />
                 </a>
                 <div className="-mr-2 flex items-center md:hidden">
                   <Popover.Button className="bg-gray-800 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
